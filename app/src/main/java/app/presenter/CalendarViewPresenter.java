2109a69701902acd3b752c51f76e4c3b86f70a6e
filipsl <filip@filip>
2019-11-30 20:14:41 +0100

    Add MonthView to CalendarView

--- app/src/main/java/app/presenter/CalendarViewPresenter.java
+++ app/src/main/java/app/presenter/CalendarViewPresenter.java
@@ -1,14 +1,61 @@
 package app.presenter;
 
+import app.view.month.MonthView;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Tab;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+import javafx.event.ActionEvent;
+
+import java.time.LocalDate;
+import java.time.YearMonth;
 
 public class CalendarViewPresenter {
-    public HBox root;
-    public Tab dayView;
-    public Tab weekView;
-    public Tab monthView;
-    public Tab yearView;
 
+    @FXML
+    public HBox root;
+
+    @FXML
+    public Tab dayViewTab;
+
+    @FXML
+    public Tab weekViewTab;
+
+    @FXML
+    public Tab monthViewTab;
+
+    @FXML
+    public DatePicker datePicker;
+
+    @FXML
+    public Button setCurrentDateButton;
+
+    private LocalDate selectedDate;
+
+    public void setSelectedDate(LocalDate selectedDate){
+        this.selectedDate = selectedDate;
+        datePicker.setValue(selectedDate);
+        setMonthViewContent();
+    }
+
+    public void handleDatePickerChange(ActionEvent actionEvent){
+        selectedDate = datePicker.getValue();
+        setMonthViewContent();
+    }
+
+    public void handleSetCurrentDateButton(ActionEvent actionEvent){
+        selectedDate = LocalDate.now();
+        datePicker.setValue(selectedDate);
+    }
+
+    public void setMonthViewContent(){
+        YearMonth yearMonth = YearMonth.of(selectedDate.getYear(), selectedDate.getMonth());
+        MonthView monthViewContent = new MonthView(yearMonth);
+        VBox monthTabContent = monthViewContent.getMonthViewVBox();
+        monthViewTab.setContent(monthTabContent);
+    }
 
 }
